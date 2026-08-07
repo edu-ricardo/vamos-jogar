5b6f2a26d66661eeb95c8d8be9a28b39f3b70eff
eduardo.florencio <eduardo.florencio@linx.com.br>
2026-08-07 13:14:42 -0300

    feat: add support for FIREBASE_SERVICE_ACCOUNT_KEY in api

--- apps/api/src/lib/firebase-admin.ts
+++ apps/api/src/lib/firebase-admin.ts
@@ -1,14 +1,24 @@
-import { initializeApp } from 'firebase-admin/app';
+import { initializeApp, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import { getAuth } from 'firebase-admin/auth';
 import dotenv from 'dotenv';
 dotenv.config();
 
 try {
-  initializeApp({
-    projectId: 'vamos-jogar-31b9b'
-  });
-} catch (e) {}
+  if (process.env.FIREBASE_SERVICE_ACCOUNT_KEY) {
+    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+    initializeApp({
+      credential: cert(serviceAccount),
+      projectId: 'vamos-jogar-31b9b'
+    });
+  } else {
+    initializeApp({
+      projectId: 'vamos-jogar-31b9b'
+    });
+  }
+} catch (e) {
+  console.error('Erro ao inicializar Firebase Admin:', e);
+}
 
 export const db = getFirestore();
 export const auth = getAuth();
